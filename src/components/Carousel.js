efde2799731f319c973885191e816cb4712bdb71
kalen-oyphanith <kmo6884@g.rit.edu>
2022-04-12 01:19:12 -0400

    update slider

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,5 +1,5 @@
 import React, {useRef} from 'react';
-import Slider from 'react-slick';
+import {Slider} from 'react-slick';
 import "bootstrap/dist/css/bootstrap.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
